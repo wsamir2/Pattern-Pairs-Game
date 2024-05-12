06e6819d6fe41ab93ea11dbd38a3b4bd1091a47b
عبدالله وليد سمير سيد <12422021638201@pg.cu.edu.eg>
2024-05-12 21:36:06 +0300

    Update Match Function

--- Pattern Pairs Game/frmMain.Designer.cs
+++ Pattern Pairs Game/frmMain.Designer.cs
@@ -29,28 +29,65 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.disposeCardTimer = new System.Windows.Forms.Timer(this.components);
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTimeLeft = new System.Windows.Forms.Label();
+            this.btnRestartGame = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // disposeCardTimer
             // 
             this.disposeCardTimer.Interval = 750;
-            this.disposeCardTimer.Tick += new System.EventHandler(this.disposeCardTimer_Tick);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(25, 282);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "label1";
+            // 
+            // lblTimeLeft
+            // 
+            this.lblTimeLeft.AutoSize = true;
+            this.lblTimeLeft.Location = new System.Drawing.Point(230, 282);
+            this.lblTimeLeft.Name = "lblTimeLeft";
+            this.lblTimeLeft.Size = new System.Drawing.Size(35, 13);
+            this.lblTimeLeft.TabIndex = 1;
+            this.lblTimeLeft.Text = "label2";
+            // 
+            // btnRestartGame
+            // 
+            this.btnRestartGame.Image = ((System.Drawing.Image)(resources.GetObject("btnRestartGame.Image")));
+            this.btnRestartGame.Location = new System.Drawing.Point(125, 272);
+            this.btnRestartGame.Name = "btnRestartGame";
+            this.btnRestartGame.Size = new System.Drawing.Size(41, 38);
+            this.btnRestartGame.TabIndex = 2;
+            this.btnRestartGame.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(313, 315);
+            this.Controls.Add(this.btnRestartGame);
+            this.Controls.Add(this.lblTimeLeft);
+            this.Controls.Add(this.lblStatus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
             this.Text = "Pattern Pairs Game";
-            this.Load += new System.EventHandler(this.frmMain_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer disposeCardTimer;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTimeLeft;
+        private System.Windows.Forms.Button btnRestartGame;
     }
 }
--- Pattern Pairs Game/frmMain.cs
+++ Pattern Pairs Game/frmMain.cs
@@ -12,24 +12,152 @@ namespace Pattern_Pairs_Game
 {
     public partial class frmMain : Form
     {
+        List<int> numbers = new List<int> { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6 ,7,7,8,8};
+        string firstChoice;
+        string secondChoice;
+        int tries;
+        List<PictureBox> pictures = new List<PictureBox>();
+        PictureBox picA;
+        PictureBox picB;
+        int totalTime = 30;
+        int countDownTime;
+        bool gameOver = false;
         public frmMain()
         {
             InitializeComponent();
+            LoadPictures();
         }
-
-        private void label1_Click(object sender, EventArgs e)
+        private void TimerEvent(object sender, EventArgs e)
         {
-
+            countDownTime--;
+            lblTimeLeft.Text = "Time Left: " + countDownTime;
+            if (countDownTime < 1)
+            {
+                GameOver("Times Up, You Lose");
+                foreach (PictureBox x in pictures)
+                {
+                    if (x.Tag != null)
+                    {
+                        x.Image = Image.FromFile("pics/" + (string)x.Tag + ".png");
+                    }
+                }
+            }
         }
-
-        private void frmMain_Load(object sender, EventArgs e)
+        private void RestartGameEvent(object sender, EventArgs e)
         {
-
+            RestartGame();
         }
-
-        private void disposeCardTimer_Tick(object sender, EventArgs e)
+        private void LoadPictures()
         {
-
+            int leftPos = 20;
+            int topPos = 20;
+            int rows = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 50;
+                newPic.Width = 50;
+                newPic.BackColor = Color.LightGray;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_Click;
+                pictures.Add(newPic);
+                if (rows < 4)
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 60;
+                }
+                if (rows == 4)
+                {
+                    leftPos = 20;
+                    topPos += 60;
+                    rows = 0;
+                }
+            }
+            RestartGame();
+        }
+        private void NewPic_Click(object sender, EventArgs e)
+        {
+            if (gameOver)
+            {
+                // dont register a click if the game is over
+                return;
+            }
+            if (firstChoice == null)
+            {
+                picA = sender as PictureBox;
+                if (picA.Tag != null && picA.Image == null)
+                {
+                    picA.Image = Image.FromFile("images/" + (string)picA.Tag + ".png");
+                    firstChoice = (string)picA.Tag;
+                }
+            }
+            else if (secondChoice == null)
+            {
+                picB = sender as PictureBox;
+                if (picB.Tag != null && picB.Image == null)
+                {
+                    picB.Image = Image.FromFile("images/" + (string)picB.Tag + ".png");
+                    secondChoice = (string)picB.Tag;
+                }
+            }
+            else
+            {
+                CheckPictures(picA, picB);
+            }
+        }
+        private void RestartGame()
+        {
+            // randomise the original list
+            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList();
+            // assign the random list to the original
+            numbers = randomList;
+            for (int i = 0; i < pictures.Count; i++)
+            {
+                pictures[i].Image = null;
+                pictures[i].Tag = numbers[i].ToString();
+            }
+            tries = 0;
+            lblStatus.Text = "Mismatched: " + tries + " times.";
+            lblTimeLeft.Text = "Time Left: " + totalTime;
+            gameOver = false;
+            disposeCardTimer.Start();
+            countDownTime = totalTime;
+        }
+        private void CheckPictures(PictureBox A, PictureBox B)
+        {
+            if (firstChoice == secondChoice)
+            {
+                A.Tag = null;
+                B.Tag = null;
+            }
+            else
+            {
+                tries++;
+                lblStatus.Text = "Mismatched " + tries + " times.";
+            }
+            firstChoice = null;
+            secondChoice = null;
+            foreach (PictureBox pics in pictures.ToList())
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+            // now lets check if all of the items have been solved
+            if (pictures.All(o => o.Tag == pictures[0].Tag))
+            {
+                GameOver("Great Work, You Win!!!!");
+            }
+        }
+        private void GameOver(string msg)
+        {
+             disposeCardTimer.Stop();
+            gameOver = true;
+            MessageBox.Show(msg + " Click Restart to Play Again.", "Walid Team Sayes: ");
         }
     }
 }
