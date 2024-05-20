be996b3cfb764104446bdf5800e73cbf3fa72a32
manaarahmed20 <manaarahmed20@gmail.com>
2024-05-20 19:17:21 +0300

    adjusting UI issues after merge and making the timer in minutes instead

--- Pattern Pairs Game/frmMain.Designer.cs
+++ Pattern Pairs Game/frmMain.Designer.cs
@@ -17,7 +17,7 @@
         private System.Windows.Forms.Panel headerPanel;
         private System.Windows.Forms.Panel footerPanel;
         private System.Windows.Forms.PictureBox logoPictureBox;
-        private System.Windows.Forms.Label TimerInSeconds;
+        private System.Windows.Forms.Label TimerInMinutes;
         private System.Windows.Forms.NumericUpDown numericUpDownTimer;
 
         protected override void Dispose(bool disposing)
@@ -44,14 +44,13 @@
             this.labelColumns = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
-            this.TimerInSeconds = new System.Windows.Forms.Label();
+            this.TimerInMinutes = new System.Windows.Forms.Label();
             this.headerPanel = new System.Windows.Forms.Panel();
             this.footerPanel = new System.Windows.Forms.Panel();
             this.logoPictureBox = new System.Windows.Forms.PictureBox();
             this.numericUpDownTimer = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColumns)).BeginInit();
-            this.tableLayoutPanel.SuspendLayout();
             this.headerPanel.SuspendLayout();
             this.footerPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
@@ -73,9 +72,10 @@
             this.lblStatus.AutoSize = true;
             this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.lblStatus.ForeColor = System.Drawing.Color.Red;
-            this.lblStatus.Location = new System.Drawing.Point(15, 16);
+            this.lblStatus.Location = new System.Drawing.Point(20, 20);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblStatus.Name = "lblStatus";
-            this.lblStatus.Size = new System.Drawing.Size(160, 17);
+            this.lblStatus.Size = new System.Drawing.Size(189, 20);
             this.lblStatus.TabIndex = 0;
             this.lblStatus.Text = "Mismatched: 0 times.";
             // 
@@ -84,32 +84,35 @@
             this.lblTimeLeft.AutoSize = true;
             this.lblTimeLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.lblTimeLeft.ForeColor = System.Drawing.Color.Green;
-            this.lblTimeLeft.Location = new System.Drawing.Point(280, 16);
+            this.lblTimeLeft.Location = new System.Drawing.Point(373, 20);
+            this.lblTimeLeft.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTimeLeft.Name = "lblTimeLeft";
-            this.lblTimeLeft.Size = new System.Drawing.Size(178, 17);
+            this.lblTimeLeft.Size = new System.Drawing.Size(207, 20);
             this.lblTimeLeft.TabIndex = 1;
             this.lblTimeLeft.Text = "Time Left: 120 seconds";
             // 
             // btnRestartGame
             // 
             this.btnRestartGame.Image = ((System.Drawing.Image)(resources.GetObject("btnRestartGame.Image")));
-            this.btnRestartGame.Location = new System.Drawing.Point(3, 0);
+            this.btnRestartGame.Location = new System.Drawing.Point(4, 0);
+            this.btnRestartGame.Margin = new System.Windows.Forms.Padding(4);
             this.btnRestartGame.Name = "btnRestartGame";
-            this.btnRestartGame.Size = new System.Drawing.Size(41, 38);
+            this.btnRestartGame.Size = new System.Drawing.Size(55, 47);
             this.btnRestartGame.TabIndex = 2;
             this.btnRestartGame.UseVisualStyleBackColor = true;
             this.btnRestartGame.Click += new System.EventHandler(this.btnRestartGame_Click);
             // 
             // numericUpDownRows
             // 
-            this.numericUpDownRows.Location = new System.Drawing.Point(76, 80);
+            this.numericUpDownRows.Location = new System.Drawing.Point(74, 81);
+            this.numericUpDownRows.Margin = new System.Windows.Forms.Padding(4);
             this.numericUpDownRows.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.numericUpDownRows.Name = "numericUpDownRows";
-            this.numericUpDownRows.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDownRows.Size = new System.Drawing.Size(120, 22);
             this.numericUpDownRows.TabIndex = 3;
             this.numericUpDownRows.Value = new decimal(new int[] {
             1,
@@ -119,24 +122,29 @@
             // 
             // numericUpDownColumns
             // 
-            this.numericUpDownColumns.Location = new System.Drawing.Point(319, 78);
+            this.numericUpDownColumns.Location = new System.Drawing.Point(318, 80);
+            this.numericUpDownColumns.Margin = new System.Windows.Forms.Padding(4);
             this.numericUpDownColumns.Minimum = new decimal(new int[] {
             1,
             0,
             0,
             0});
             this.numericUpDownColumns.Name = "numericUpDownColumns";
-            this.numericUpDownColumns.Size = new System.Drawing.Size(90, 20);
+            this.numericUpDownColumns.Size = new System.Drawing.Size(120, 22);
             this.numericUpDownColumns.TabIndex = 4;
+            this.numericUpDownColumns.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // labelRows
             // 
             this.labelRows.AutoSize = true;
             this.labelRows.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
-            this.labelRows.Location = new System.Drawing.Point(9, 65);
-            this.labelRows.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelRows.Location = new System.Drawing.Point(12, 80);
             this.labelRows.Name = "labelRows";
-            this.labelRows.Size = new System.Drawing.Size(46, 17);
+            this.labelRows.Size = new System.Drawing.Size(55, 20);
             this.labelRows.TabIndex = 5;
             this.labelRows.Text = "Rows";
             // 
@@ -144,10 +152,9 @@
             // 
             this.labelColumns.AutoSize = true;
             this.labelColumns.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
-            this.labelColumns.Location = new System.Drawing.Point(166, 65);
-            this.labelColumns.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.labelColumns.Location = new System.Drawing.Point(215, 81);
             this.labelColumns.Name = "labelColumns";
-            this.labelColumns.Size = new System.Drawing.Size(69, 17);
+            this.labelColumns.Size = new System.Drawing.Size(82, 20);
             this.labelColumns.TabIndex = 6;
             this.labelColumns.Text = "Columns";
             // 
@@ -155,10 +162,10 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
             this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
-            this.button1.Location = new System.Drawing.Point(530, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Location = new System.Drawing.Point(852, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(41, 38);
+            this.button1.Size = new System.Drawing.Size(55, 47);
             this.button1.TabIndex = 7;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -170,26 +177,27 @@
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel.Controls.Add(this.TimerInSeconds, 2, 0);
-            this.tableLayoutPanel.Location = new System.Drawing.Point(12, 108);
+            this.tableLayoutPanel.Location = new System.Drawing.Point(74, 180);
+            this.tableLayoutPanel.Margin = new System.Windows.Forms.Padding(4);
             this.tableLayoutPanel.Name = "tableLayoutPanel";
             this.tableLayoutPanel.RowCount = 4;
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel.Size = new System.Drawing.Size(570, 325);
+            this.tableLayoutPanel.Size = new System.Drawing.Size(760, 400);
             this.tableLayoutPanel.TabIndex = 8;
             // 
-            // TimerInSeconds
+            // TimerInMinutes
             // 
-            this.TimerInSeconds.AutoSize = true;
-            this.TimerInSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
-            this.TimerInSeconds.Location = new System.Drawing.Point(383, 0);
-            this.TimerInSeconds.Name = "TimerInSeconds";
-            this.TimerInSeconds.Size = new System.Drawing.Size(178, 40);
-            this.TimerInSeconds.TabIndex = 13;
-            this.TimerInSeconds.Text = "Enter timeout value in seconds";
+            this.TimerInMinutes.AutoSize = true;
+            this.TimerInMinutes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.TimerInMinutes.Location = new System.Drawing.Point(461, 83);
+            this.TimerInMinutes.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TimerInMinutes.Name = "TimerInMinutes";
+            this.TimerInMinutes.Size = new System.Drawing.Size(269, 20);
+            this.TimerInMinutes.TabIndex = 13;
+            this.TimerInMinutes.Text = "Maximum allowed time (mins)";
             // 
             // headerPanel
             // 
@@ -198,9 +206,9 @@
             this.headerPanel.Controls.Add(this.lblStatus);
             this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.headerPanel.Location = new System.Drawing.Point(0, 0);
-            this.headerPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.headerPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.headerPanel.Name = "headerPanel";
-            this.headerPanel.Size = new System.Drawing.Size(588, 41);
+            this.headerPanel.Size = new System.Drawing.Size(919, 50);
             this.headerPanel.TabIndex = 9;
             // 
             // footerPanel
@@ -209,40 +217,47 @@
             this.footerPanel.Controls.Add(this.btnRestartGame);
             this.footerPanel.Controls.Add(this.button1);
             this.footerPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.footerPanel.Location = new System.Drawing.Point(0, 415);
-            this.footerPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.footerPanel.Location = new System.Drawing.Point(0, 640);
+            this.footerPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.footerPanel.Name = "footerPanel";
-            this.footerPanel.Size = new System.Drawing.Size(588, 41);
+            this.footerPanel.Size = new System.Drawing.Size(919, 50);
             this.footerPanel.TabIndex = 10;
             // 
             // logoPictureBox
             // 
             this.logoPictureBox.BackColor = System.Drawing.Color.LightBlue;
-            this.logoPictureBox.Location = new System.Drawing.Point(530, 0);
-            this.logoPictureBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.logoPictureBox.Location = new System.Drawing.Point(707, 0);
+            this.logoPictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.logoPictureBox.Name = "logoPictureBox";
-            this.logoPictureBox.Size = new System.Drawing.Size(58, 41);
+            this.logoPictureBox.Size = new System.Drawing.Size(77, 50);
             this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.logoPictureBox.TabIndex = 11;
             this.logoPictureBox.TabStop = false;
             // 
             // numericUpDownTimer
             // 
-            this.numericUpDownTimer.Location = new System.Drawing.Point(599, 78);
-            this.numericUpDownTimer.Name = "numericUpDownTimer";
-            this.numericUpDownTimer.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDownTimer.TabIndex = 12;
+            this.numericUpDownTimer.Location = new System.Drawing.Point(759, 82);
+            this.numericUpDownTimer.Margin = new System.Windows.Forms.Padding(4);
             this.numericUpDownTimer.Minimum = new decimal(new int[] {
-            60,
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownTimer.Name = "numericUpDownTimer";
+            this.numericUpDownTimer.Size = new System.Drawing.Size(160, 22);
+            this.numericUpDownTimer.TabIndex = 12;
+            this.numericUpDownTimer.Value = new decimal(new int[] {
+            1,
             0,
             0,
             0});
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(919, 690);
+            this.Controls.Add(this.TimerInMinutes);
             this.Controls.Add(this.numericUpDownTimer);
             this.Controls.Add(this.logoPictureBox);
             this.Controls.Add(this.tableLayoutPanel);
@@ -253,12 +268,11 @@
             this.Controls.Add(this.headerPanel);
             this.Controls.Add(this.footerPanel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmMain";
             this.Text = "Pattern Pairs Game";
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRows)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownColumns)).EndInit();
-            this.tableLayoutPanel.ResumeLayout(false);
-            this.tableLayoutPanel.PerformLayout();
             this.headerPanel.ResumeLayout(false);
             this.headerPanel.PerformLayout();
             this.footerPanel.ResumeLayout(false);
